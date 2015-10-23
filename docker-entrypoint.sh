a578451f20419509eda96041e2a99afb8a44218f
Vartan Simonian <vsimonianpress@gmail.com>
2015-10-22 17:59:10 -0700

    Don't chown secrets directory
    Fixes #1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ "$1" = "anvil-connect" ]; then
-	chown -R connect .
+	chown -R connect $(ls | awk '{if($1 != "secrets"){ print $1 }}')
 	exec gosu connect "$@"
 elif echo "$1" | egrep -q '^--'; then
-	chown -R connect .
+	chown -R connect $(ls | awk '{if($1 != "secrets"){ print $1 }}')
 	exec gosu connect "anvil-connect" "$@"
 fi
 
